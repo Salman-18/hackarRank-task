41f5eca9a7ef7fa3d9c189f84b5a9e01da93fd92
Salman-18 <salmannizamani205@gmail.com>
2023-03-10 10:49:30 +0500

    first commit

---
+++ python if-else.py
@@ -0,0 +1,21 @@
+"""Given an integer,n , perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5 , print Not Weird
+If n is even and in the inclusive range of 6 to 20 , print Weird
+If n is even and greater than 20 , print Not Weird"""
+import math
+import os
+import random
+import re
+import sys
+
+n = int(input())
+if n % 2 != 0:
+    print("Weird")
+elif n % 2 == 0 and range(2, 5):
+    print("Not Weird")
+elif n % 2 == 0 and range(6, 20):
+    print("Weird")
+elif n % 2 == 0 and n > 20:
+    print("Not Weird")
